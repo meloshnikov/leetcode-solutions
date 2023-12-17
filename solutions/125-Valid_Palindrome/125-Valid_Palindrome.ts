ab443b0972d42bb67c604afd593fc0046293cef4
meloshnikov <meloshnikoff@gmail.com>
2023-12-17 14:51:10 +0300

    Runtime: 63ms Beats 75.09% of users with TypeScript.

---
+++ solutions/125-Valid_Palindrome/125-Valid_Palindrome.ts
@@ -0,0 +1,38 @@
+/** 125. Valid Palindrome **/
+
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+*/
+
+
+/*
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+*/
+
+
+/**  Solution **/
+
+const isPalindrome = (s: string): boolean => {
+    const cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
+  };
