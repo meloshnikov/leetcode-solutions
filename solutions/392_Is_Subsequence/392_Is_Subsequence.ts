146af85c87c09b2d2fdad6c4f9cdf01883a9fadd
meloshnikov <meloshnikoff@gmail.com>
2023-12-18 13:55:00 +0300

    Runtime: 54ms Beats 71.25% of users with TypeScript.

---
+++ solutions/392_Is_Subsequence/392_Is_Subsequence.ts
@@ -0,0 +1,32 @@
+/** 392. Is Subsequence **/
+
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters
+without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+*/
+
+
+/*
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+*/
+
+
+/**  Solution **/
+
+const isSubsequence = (sub: string, string: string): boolean => {
+    let index = 0;
+    for (const letter of string) {
+        if (letter === sub[index]) index++;
+    }
+    return index === sub.length
+};
